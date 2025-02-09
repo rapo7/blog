c9f9c1384d00931d3664898b1c924298c88c4f13
rapo7 <144160473+rapo7@users.noreply.github.com>
2025-02-09 10:21:54 -0500

    actiona chnage working - minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   image: {
     service: sharpImageService()
   },
-  site: "https://cojocarudavid.me",
+  site: 'https://rapo7.github.io',
   vite: {
     plugins: [rawFonts([".ttf", ".woff"])],
     optimizeDeps: {
